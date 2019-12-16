5640a7a3bc3082566c5f4d286848fb3489a35326
Astramentis <WesleyFLVS@gmail.com>
2019-12-16 16:39:15 -0500

    Added submission and test
    Prevent default stops page refresh

--- src/AddTodo/AddTodo.js
+++ src/AddTodo/AddTodo.js
@@ -4,13 +4,18 @@ class AddTodo extends React.Component{
     render (){
         return(
         <div className = 'AddTodoContainer'>
-            <form>  
+            <form onSubmit={(e)=> this.submitTodo(e)}>  
                 <input onChange = {(e)=> this.updateInput(e)} type = 'text'></input>
+                <button type = 'submit'>Add Todo</button>
             </form>
         </div>)
     }
     updateInput = (e) => {
         console.log(e);
+    }
+    submitTodo = (e) => {
+        e.preventDefault();
+        console.log('submit');
     }
 
 }
